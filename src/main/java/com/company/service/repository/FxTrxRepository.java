778f77cefdd06d566d47c5101f9c6367bcdc969a
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 00:38:46 +0800

    Use custom FX enquiry repository

--- src/main/java/com/company/service/repository/FxTrxRepository.java
+++ src/main/java/com/company/service/repository/FxTrxRepository.java
@@ -3,28 +3,15 @@ package com.company.service.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.company.service.api.dto.FxEnquiryDto;
 import com.company.service.entity.FxTrx;
 
-public interface FxTrxRepository extends JpaRepository<FxTrx, Long> {
+public interface FxTrxRepository extends JpaRepository<FxTrx, Long>, FxTrxRepositoryCustom {
 
     List<FxTrx> findByMasterIdOrderByRecordNoAsc(Long masterId);
 
     Optional<FxTrx> findTopByMasterIdOrderByRecordNoDesc(Long masterId);
 
     void deleteByMasterId(Long masterId);
-
-    @Query(
-        value = "select new com.company.service.api.dto.FxEnquiryDto(" +
-            "m.reportDate, t.recordNo, t.fxCategory, t.fxCode, t.fxType, t.fxAmount, t.fxDate) " +
-            "from FxTrx t, FxMaster m " +
-            "where m.id = t.masterId",
-        countQuery = "select count(t) from FxTrx t, FxMaster m where m.id = t.masterId"
-    )
-    Page<FxEnquiryDto> findEnquiry(Pageable pageable);
 }
