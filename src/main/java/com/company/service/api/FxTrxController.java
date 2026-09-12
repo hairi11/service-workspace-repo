82e77cb05f7c0439d8e42b00c444bb676a94062e
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 16:21:18 +0800

    Use GET and POST only for transaction API

--- src/main/java/com/company/service/api/FxTrxController.java
+++ src/main/java/com/company/service/api/FxTrxController.java
@@ -3,11 +3,9 @@ package com.company.service.api;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,12 +40,12 @@ public class FxTrxController {
         return service.findById(id);
     }
 
-    @PutMapping("/fx-transactions/{id}")
+    @PostMapping("/fx-transactions/{id}")
     public FxTrx update(@PathVariable Long id, @RequestBody FxTrx input) {
         return service.update(id, input);
     }
 
-    @DeleteMapping("/fx-transactions/{id}")
+    @PostMapping("/fx-transactions/{id}/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         service.delete(id);
