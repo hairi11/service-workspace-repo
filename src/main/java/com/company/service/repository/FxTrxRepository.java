8d0fa692a2458d77f846e1bf09926801cfdf2bfb
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 23:53:26 +0800

    Add paged FX enquiry query

--- src/main/java/com/company/service/repository/FxTrxRepository.java
+++ src/main/java/com/company/service/repository/FxTrxRepository.java
@@ -3,8 +3,12 @@ package com.company.service.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.company.service.api.dto.FxEnquiryDto;
 import com.company.service.entity.FxTrx;
 
 public interface FxTrxRepository extends JpaRepository<FxTrx, Long> {
@@ -14,4 +18,14 @@ public interface FxTrxRepository extends JpaRepository<FxTrx, Long> {
     Optional<FxTrx> findTopByMasterIdOrderByRecordNoDesc(Long masterId);
 
     void deleteByMasterId(Long masterId);
+
+    @Query(
+        value = "select new com.company.service.api.dto.FxEnquiryDto(" +
+            "m.reportDate, t.recordNo, t.fxCategory, t.fxCode, t.fxType, t.fxAmount, t.fxDate) " +
+            "from FxTrx t, FxMaster m " +
+            "where m.id = t.masterId " +
+            "order by m.reportDate desc, t.recordNo asc",
+        countQuery = "select count(t) from FxTrx t, FxMaster m where m.id = t.masterId"
+    )
+    Page<FxEnquiryDto> findEnquiry(Pageable pageable);
 }
