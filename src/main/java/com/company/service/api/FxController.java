23158f28fe56d9fd9ed0b4b89bfb8acf6a236acc
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 16:42:16 +0800

    Add combined FX save API

---
+++ src/main/java/com/company/service/api/FxController.java
@@ -0,0 +1,26 @@
+package com.company.service.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.service.api.dto.FxSaveRequest;
+import com.company.service.api.dto.FxSaveResponse;
+import com.company.service.service.FxService;
+
+@RestController
+@RequestMapping("/api/fx")
+public class FxController {
+
+    private final FxService service;
+
+    public FxController(FxService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/save")
+    public FxSaveResponse save(@RequestBody FxSaveRequest request) {
+        return service.save(request);
+    }
+}
