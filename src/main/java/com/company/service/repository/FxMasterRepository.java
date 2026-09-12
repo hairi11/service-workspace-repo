c97a1d0d2bb96c3cb944d35cdbf303e2f1c1b1e5
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 16:16:05 +0800

    Add FX master repository

---
+++ src/main/java/com/company/service/repository/FxMasterRepository.java
@@ -0,0 +1,8 @@
+package com.company.service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.company.service.entity.FxMaster;
+
+public interface FxMasterRepository extends JpaRepository<FxMaster, Long> {
+}
