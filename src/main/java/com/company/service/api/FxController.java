8f87e80c5eec479c826ed967f937c265fc5bd577
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 22:01:04 +0800

    Stop exposing FX entities from controller

--- src/main/java/com/company/service/api/FxController.java
+++ src/main/java/com/company/service/api/FxController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.service.api.dto.FxMasterDto;
 import com.company.service.api.dto.FxSaveRequest;
 import com.company.service.api.dto.FxSaveResponse;
-import com.company.service.entity.FxMaster;
-import com.company.service.entity.FxTrx;
+import com.company.service.api.dto.FxTrxDto;
 import com.company.service.service.FxService;
 
 @RestController
@@ -28,23 +28,23 @@ public class FxController {
     }
 
     @GetMapping("/fx-masters")
-    public List<FxMaster> findAllMasters() {
+    public List<FxMasterDto> findAllMasters() {
         return service.findAllMasters();
     }
 
     @GetMapping("/fx-masters/{id}")
-    public FxMaster findMasterById(@PathVariable Long id) {
+    public FxMasterDto findMasterById(@PathVariable Long id) {
         return service.findMasterById(id);
     }
 
     @PostMapping("/fx-masters")
     @ResponseStatus(HttpStatus.CREATED)
-    public FxMaster createMaster(@RequestBody FxMaster input) {
+    public FxMasterDto createMaster(@RequestBody FxMasterDto input) {
         return service.createMaster(input);
     }
 
     @PostMapping("/fx-masters/{id}")
-    public FxMaster updateMaster(@PathVariable Long id, @RequestBody FxMaster input) {
+    public FxMasterDto updateMaster(@PathVariable Long id, @RequestBody FxMasterDto input) {
         return service.updateMaster(id, input);
     }
 
@@ -55,23 +55,23 @@ public class FxController {
     }
 
     @GetMapping("/fx-masters/{masterId}/transactions")
-    public List<FxTrx> findTransactionsByMasterId(@PathVariable Long masterId) {
+    public List<FxTrxDto> findTransactionsByMasterId(@PathVariable Long masterId) {
         return service.findTransactionsByMasterId(masterId);
     }
 
     @PostMapping("/fx-masters/{masterId}/transactions")
     @ResponseStatus(HttpStatus.CREATED)
-    public FxTrx createTransaction(@PathVariable Long masterId, @RequestBody FxTrx input) {
+    public FxTrxDto createTransaction(@PathVariable Long masterId, @RequestBody FxTrxDto input) {
         return service.createTransaction(masterId, input);
     }
 
     @GetMapping("/fx-transactions/{id}")
-    public FxTrx findTransactionById(@PathVariable Long id) {
+    public FxTrxDto findTransactionById(@PathVariable Long id) {
         return service.findTransactionById(id);
     }
 
     @PostMapping("/fx-transactions/{id}")
-    public FxTrx updateTransaction(@PathVariable Long id, @RequestBody FxTrx input) {
+    public FxTrxDto updateTransaction(@PathVariable Long id, @RequestBody FxTrxDto input) {
         return service.updateTransaction(id, input);
     }
 
