3c893f06d18b644a324377f95a287f0383a2a6e9
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 01:28:05 +0800

    Add FX reference type constants

---
+++ src/main/java/com/company/service/common/FxReferenceType.java
@@ -0,0 +1,12 @@
+package com.company.service.common;
+
+public final class FxReferenceType {
+
+    public static final String CATEGORY = "FX_CATEGORY";
+    public static final String CODE = "FX_CODE";
+    public static final String CURRENCY = "FX_CURRENCY";
+    public static final String TYPE = "FX_TYPE";
+
+    private FxReferenceType() {
+    }
+}
