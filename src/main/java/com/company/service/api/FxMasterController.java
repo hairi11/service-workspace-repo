2464842ac1adb2eed99248f8fe43945fe0f9f200
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 16:21:04 +0800

    Use GET and POST only for master API

--- src/main/java/com/company/service/api/FxMasterController.java
+++ src/main/java/com/company/service/api/FxMasterController.java
@@ -3,11 +3,9 @@ package com.company.service.api;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,12 +40,12 @@ public class FxMasterController {
         return service.create(input);
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}")
     public FxMaster update(@PathVariable Long id, @RequestBody FxMaster input) {
         return service.update(id, input);
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping("/{id}/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         service.delete(id);
